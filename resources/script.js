5915e679f5fac97725e5b648e99f69b806419a92
constantin leitermann <consti.leitermann@icloud.com>
2024-01-03 23:19:50 +0100

    .

--- resources/script.js
+++ resources/script.js
@@ -246,6 +246,7 @@ async function processlogin(response, content){
       const responseHash = data[2]
 
       if (!loginSuccess){
+        deleteAllCookies()
         console.log("couldn't login")
         document.getElementById("loginRes").textContent = "Error:  " + responseStr
         resolve()
@@ -412,6 +413,23 @@ async function getCookie(cname) {
     }
     resolve([false,"no cookie"]) ;
   })
+}
+
+function deleteAllCookies() {
+  var cookies = document.cookie.split("; ");
+  for (var c = 0; c < cookies.length; c++) {
+      var d = window.location.hostname.split(".");
+      while (d.length > 0) {
+          var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+          var p = location.pathname.split('/');
+          document.cookie = cookieBase + '/';
+          while (p.length > 0) {
+              document.cookie = cookieBase + p.join('/');
+              p.pop();
+          };
+          d.shift();
+      }
+  }
 }
 
 async function checkLogin() {
