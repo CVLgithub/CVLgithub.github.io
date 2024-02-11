df002dc2791088a572ca2a3539462e7ee7357b0d
constantin leitermann <consti.leitermann@icloud.com>
2024-02-11 23:18:40 +0100

    .

--- resources/script.js
+++ resources/script.js
@@ -433,9 +433,7 @@ function deleteAllCookies() {
                 // Set to "Thu, 01 Jan 1970 00:00:00 GMT" 
                 for (var i = 0; i < allCookies.length; i++) 
                     document.cookie = allCookies[i] + "=;expires=" 
-                    + new Date(0).toUTCString(); 
-  
-                displayCookies.innerHTML = document.cookie;
+                    + new Date(0).toUTCString();  
 }
 
 async function checkLogin() {
